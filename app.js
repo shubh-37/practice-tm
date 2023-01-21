76260bab97ee4a884f9fea9807e86b2ddab6d1fa
shubh-37 <shubh.arya37@gmail.com>
2023-01-21 22:04:02 +0530

    middleware setup for all routes

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
+const tasks = require("./routes/tasks");
 
-app.get("/",(req,res)=>{
-    res.send("Welcome!")
-})
+app.use("api/v1/tasks",tasks); 
+app.use(express.json()); //to convert all the incoming request into json
 
 app.listen(3000, console.log("server running on port: 3000"));
