67fe5951ab6d8f922a6398f5a3c996892fea464a
shubh-37 <shubh.arya37@gmail.com>
2023-01-22 15:07:38 +0530

    invoked the connection

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
 
 app.use("/api/v1/tasks",tasks); 
 app.use(express.json()); //to convert all the incoming request into json
 
-app.listen(3000, console.log("server running on port: 3000"));
+
+const start = async() => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(3000, console.log("server running on port: 3000"));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
+
