44141eeffe9a68b41d6a89950c09a65463f8a185
shubh-37 <shubh.arya37@gmail.com>
2023-01-22 13:08:54 +0530

    imported all the controllers

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
+const {
+    getAllTasks,
+    createTask,
+    getATask,
+    updateTask,
+    deleteTask
+} = require("../controllers/task");
+
 router.route("/").get(getAllTasks).post(createTask);
 router.route("/:id").get(getATask).patch(updateTask).delete(deleteTask);
 
