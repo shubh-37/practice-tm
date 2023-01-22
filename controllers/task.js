602c318648f267da202494909f27c18ec71e795a
shubh-37 <shubh.arya37@gmail.com>
2023-01-22 23:35:02 +0530

    logic of all controllers

--- controllers/task.js
+++ controllers/task.js
@@ -1,21 +1,68 @@
-const getAllTasks = (req,res)=>{
-    res.send("All the tasks");
+const Task = require("../models/Task");
+
+const getAllTasks = async (req,res)=>{
+    try {
+        const tasks = await Task.find();
+        res.status(200).json({tasks});
+    } catch (error) {
+        res.status(500).json({msg: error});
+    }
 }
 
-const createTask = (req,res)=>{
-    res.send("creating a task");
+const createTask = async (req,res)=>{
+    try {
+        const task = await Task.create(req.body);
+        res.status(201).json({task});
+    } catch (error) {
+        res.status(500).json({msg:error});
+    }
 }
 
-const getATask = (req,res)=>{
-    res.send("get a task");
+const getATask = async(req,res) =>{
+    try {
+        const {id: taskID} = req.params;
+        const task = await Task.findOne({ _id: taskID});
+        console.log(task);
+        if(!task){
+            return res.status(404).json({ msg: `No task found with ID: ${taskID}`});
+        }
+        res.status(200).json({ task });
+    }catch(error){
+        res.status(500).json({ msg: error});
+    }    
 }
 
-const deleteTask = (req,res)=>{
-    res.send("deleting a task");
+const deleteTask = async (req,res)=>{
+    try {
+        const {id: taskID} = req.params;
+        const task = await Task.findOneAndDelete({ _id: taskID});
+        if(!task){
+            return res.status(404).json({msg : `No task exists with ID: ${taskID}`});
+        }
+        res.status(200).json({ msg: "Successfully deleted" });
+    } catch (error) {
+        res.status(500).json({ msg: error });
+    }
 }
 
-const updateTask = (req,res)=>{
-    res.send("updating a task");
+const updateTask = async (req,res)=>{
+    try {
+        const { id: taskID } = req.params;
+        const task = await Task.findOneAndUpdate({_id: taskID}, req.body, {
+            new: true,
+            runValidators: true
+        });
+        if(!task){
+            return res.status(404).json({msg: `Cannot find task with ID: ${taskID}`});
+        }
+        res.status(200).json({ task });   
+    } catch (error) {
+        res.status(500).json({msg: error});
+    }
+
+
+
+    
 }
 
 module.exports ={
